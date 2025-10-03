ef11ac2470f4b239d6d78569fb8cf088b3f67dab
Eduardo Quinalha <equinalha@mppr.mp.br>
2025-10-03 16:21:10 -0300

    Novas configurações do SpringDoc

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-
 
 // Configuração para permitir o uso de bearer token diretamente pela Swagger UI
 @Configuration
@@ -17,7 +19,17 @@ public class SpringDocConfigurations {
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Voll.med API")
+                        .description(
+                                "API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                        .contact(new Contact()
+                                .name("Time Backend")
+                                .email("backend@voll.med"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://voll.med/api/licenca")));
     }
 
 }
